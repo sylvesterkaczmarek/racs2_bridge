2152b7883c9ba7dc1bc91225d7384bdf32b581f5
Sylvester Kaczmarek <16242628+sylvesterkaczmarek@users.noreply.github.com>
2026-08-14 23:24:50 +0100

    Add explicit websocket body length field

--- cFS/Bridge/Client_C/apps/racs2_bridge_client/fsw/src/racs2_bridge_msg.h
+++ cFS/Bridge/Client_C/apps/racs2_bridge_client/fsw/src/racs2_bridge_msg.h
@@ -4,12 +4,15 @@
 /*
 ** Type definition (websocket data format for racs2 bridge)
 */
-#define BRIDGE_HEADER_LNGTH   32
-#define BODY_DATA_MAX_LNGTH   128
+#define BRIDGE_HEADER_LNGTH       32
+#define BODY_DATA_LENGTH_LNGTH     4
+#define BODY_DATA_MAX_LNGTH      128
+#define BODY_DATA_OFFSET          (BRIDGE_HEADER_LNGTH + BODY_DATA_LENGTH_LNGTH)
 
 typedef struct
 {
     uint8              header[BRIDGE_HEADER_LNGTH];
+    uint8              body_data_length[BODY_DATA_LENGTH_LNGTH];
     uint8              body_data[BODY_DATA_MAX_LNGTH];
 
 }   OS_PACK racs2_bridge_msg_t  ;
